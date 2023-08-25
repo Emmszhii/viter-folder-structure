609221d4afd4f1ce89927de724a1a4ba8788dd42
Emmszhii <emmanuelmanalo707@gmail.com>
2023-08-25 12:15:53 +0800

    Modals

--- src/component/pages/developer/settings/users/roles/RolesTable.jsx
+++ src/component/pages/developer/settings/users/roles/RolesTable.jsx
@@ -1,46 +1,131 @@
 import React from "react";
-import { AiTwotoneEdit } from "react-icons/ai";
+import { AiTwotoneEdit, AiFillDelete } from "react-icons/ai";
 import { BsArchiveFill } from "react-icons/bs";
+import { FaTrashRestoreAlt } from "react-icons/fa";
 import { roles } from "./data.js";
+import Pills from "../../../../../partials/Pills.jsx";
+import TableLoading from "../../../../../partials/TableLoading.jsx";
+import ModalConfirm from "../../../../../partials/modals/ModalConfirm.jsx";
+import ModalDeleteAndRestore from "../../../../../partials/modals/ModalDeleteAndRestore.jsx";
 
 const RolesTable = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isArchive, setIsArchive] = React.useState(false);
+  const [isDelete, setIsDelete] = React.useState(false);
+  const [isRestore, setIsRestore] = React.useState(false);
+  const [item, setItem] = React.useState([]);
+
+  const handleArchive = (item) => {
+    setItem(item);
+    setIsArchive(true);
+  };
+  const handleDelete = (item) => {
+    setItem(item);
+    setIsDelete(true);
+    setIsRestore(false);
+  };
+  const handleRestore = (item) => {
+    setItem(item);
+    setIsDelete(true); // open modal
+    setIsRestore(true);
+  };
+
+  React.useEffect(() => {
+    function loadData() {
+      setIsLoading(true),
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
+    }
+    loadData();
+  }, []);
+
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Status</th>
-            <th className="action"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {roles.map((item, key) => (
-            <tr key={key}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.description}</td>
-              <td>Active</td>
-              <td className="table__action">
-                <ul className="flex items-center gap-4">
-                  <li>
-                    <button className="tooltip" data-tooltip="Edit">
-                      <AiTwotoneEdit />
-                    </button>
-                  </li>
-                  <li>
-                    <button className="tooltip" data-tooltip="Archive">
-                      <BsArchiveFill />
-                    </button>
-                  </li>
-                </ul>
-              </td>
+      {isLoading ? (
+        <TableLoading count={20} cols={4} />
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Status</th>
+              <th className="action"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {roles.map((item, key) => {
+              return (
+                <tr key={key}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.description}</td>
+                  <td>
+                    {item.status === 1 ? (
+                      <Pills label="active" bgc="bg-green-800" />
+                    ) : (
+                      <Pills label="Inactive" bgc="bg-gray-500" />
+                    )}
+                  </td>
+                  <td className="table__action">
+                    {item.status === 1 ? (
+                      <ul className="flex items-center gap-4">
+                        <li>
+                          <button className="tooltip" data-tooltip="Edit">
+                            <AiTwotoneEdit />
+                          </button>
+                        </li>
+                        <li>
+                          <button
+                            className="tooltip"
+                            data-tooltip="Archive"
+                            onClick={() => handleArchive(item)}
+                          >
+                            <BsArchiveFill />
+                          </button>
+                        </li>
+                      </ul>
+                    ) : (
+                      <ul className="flex items-center gap-4">
+                        <li>
+                          <button
+                            className="tooltip"
+                            data-tooltip="Delete"
+                            onClick={() => handleDelete(item)}
+                          >
+                            <AiFillDelete />
+                          </button>
+                        </li>
+                        <li>
+                          <button
+                            className="tooltip"
+                            data-tooltip="Restore"
+                            onClick={() => handleRestore(item)}
+                          >
+                            <FaTrashRestoreAlt />
+                          </button>
+                        </li>
+                      </ul>
+                    )}
+                    ;
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+
+      {isArchive && <ModalConfirm setIsArchive={setIsArchive} item={item} />}
+      {isDelete && (
+        <ModalDeleteAndRestore
+          setIsDelete={setIsDelete}
+          item={item}
+          isRestore={isRestore}
+        />
+      )}
     </>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,8 +22,15 @@ export default {
             transform: "rotate(360deg)",
           },
         },
+        loading: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
       },
-      animation: { rotate: "rotate 2s linear infinite" },
+      animation: {
+        rotate: "rotate 2s linear infinite",
+        loading: "loading 1.5s ease-in infinite",
+      },
     },
   },
   plugins: [],
